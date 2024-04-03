52d0eb49a65cf8a0559a6e08117fa088e5c7a4cf
AliciaPanashe <aliciapanashe@gmail.com>
2024-04-03 20:57:42 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 function updateWeather(response) {
+  let cityInput = document.querySelector("#current-city");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
   let temperatureElement = document.querySelector("#temperature-value");
   let temperature = response.data.temperature.current;
-  let cityInput = document.querySelector("#current-city");
 
   cityInput.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -27,4 +59,4 @@ function enterCity(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", enterCity);
 
-searchCity("Paris");
+searchCity("Germiston");
