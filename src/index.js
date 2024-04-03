13410c28dcfb95bb1bc649d54ccc6bc6980f5b12
AliciaPanashe <aliciapanashe@gmail.com>
2024-04-03 02:53:43 +0200

    added api to the code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityInput = document.querySelector("#current-city");
+
+  cityInput.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  // call an API and update user interface
+
+  let apikey = "8c8ca73cb8675t3o145bd7d07f4ef1f6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  axios.get(apiUrl).then(updateWeather);
+  console.log(apiUrl);
+}
+
 function enterCity(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let cityInput = document.querySelector("#current-city");
-  cityInput.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", enterCity);
+
+searchCity("Paris");
